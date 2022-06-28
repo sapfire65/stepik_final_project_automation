6b0be7631f6086fdd3470962cb455d6d8346c69e
Sapfire65 <cot65@yandex.ru>
2022-06-29 01:53:59 +0300

    conftest.py Консольный параметр --headless=true Для выключения отображения окна браузера Firefox

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
 import pytest
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 # получает значения из консоли для --browser_name= и --language=
 def pytest_addoption(parser):
@@ -22,20 +24,24 @@ def browser(request):
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         # Чтобы указать язык браузера, использую класс Options и метод add_experimental_option
-        # Для Chrome браузера
+        # Без браузерный режим для 'Chrome'
         options = Options()
         if headless == 'true':
             options.add_argument('headless')
 
-
         # // Отключение сообщений в консоли типа: USB: usb_device_handle...
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
+        # // Выбор языка страницы
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
         browser.implicitly_wait(10) # Не явное ожидание элементов 10 сек.
+
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
+        # Без браузерный режим для 'Firefox', через импорт библиотеки 'os'
+        if headless == 'true':
+            os.environ['MOZ_HEADLESS'] = '1'
+
         # Чтобы указать язык браузера, использую класс Options и метод add_experimental_option
         # Для Firefox  браузера
         fp = webdriver.FirefoxProfile()
