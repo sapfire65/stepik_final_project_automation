45a0b1a0a1d919047cf488430d0f8995f6524cd4
Sapfire65 <cot65@yandex.ru>
2022-07-01 13:31:01 +0300

    Update: 01.07.2022 Плюсы наследования

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Родительский класс
 class BasePage():
     # Создаем конструкцию взаимодействия передачи ссылки в браузер
     def __init__(self, browser: RemoteWebDriver, url):
@@ -17,10 +18,20 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
+    # Проверяем наличие ссылки на страницу логина.
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
             "Login link is not presented"
 
+    # Если элемент найден возвращаем True, иначе перехватываем ошибку 'NoSuchElementException' и присваиваем False
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    # Кликаем по ссылке логин / регистрация
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
--- pages/main_page.py
+++ pages/main_page.py
@@ -8,22 +8,22 @@ import time
 class MainPage(BasePage):
     pass
 
-    # Кликаем по ссылке логин / регистрация
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+    # # Кликаем по ссылке логин / регистрация
+    # def go_to_login_page(self):
+    #     login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    #     login_link.click()
 
-    # Проверяем наличие ссылки на страницу логина.
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
-    # Если элемент найден возвращаем True, иначе перехватываем ошибку 'NoSuchElementException' и присваиваем False
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
-            return False
-        return True
+    # # Проверяем наличие ссылки на страницу логина.
+    # def should_be_login_link(self):
+    #     assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    #
+    # # Если элемент найден возвращаем True, иначе перехватываем ошибку 'NoSuchElementException' и присваиваем False
+    # def is_element_present(self, how, what):
+    #     try:
+    #         self.browser.find_element(how, what)
+    #     except (NoSuchElementException):
+    #         return False
+    #     return True
 
 
 
